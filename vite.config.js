f261dcf564081ff1bca520747e95813209567fb9
MinhFresher <chiminh1207@gmail.com>
2025-08-02 12:06:47 +0700

    CD

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import rollupNodePolyfills from 'rollup-plugin-node-polyfills'
 
+// https://vite.dev/config/
 export default defineConfig({
+  base: '/React-BookStore/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
-      buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
-    },
-  },
-  define: {
-    global: 'globalThis',
-    'process.env': {}, // prevents crashes when accessing process.env
-  },
-  optimizeDeps: {
-    include: ['buffer', 'process'],
-  },
-  build: {
-    rollupOptions: {
-      plugins: [rollupNodePolyfills()],
-    },
-  },
   server: {
     proxy: {
       '/api': 'http://localhost:8080/backend',
